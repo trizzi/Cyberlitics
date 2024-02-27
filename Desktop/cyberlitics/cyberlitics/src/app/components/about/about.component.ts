a26c0dfe42503f98fd7dc39fd7d60ee00b07de7b
trizzi <patrickakhamiog@gmail.com>
2024-02-27 23:53:52 +0000

    used for loop in home and about page

--- Desktop/cyberlitics/cyberlitics/src/app/components/about/about.component.ts
+++ Desktop/cyberlitics/cyberlitics/src/app/components/about/about.component.ts
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+about = {
+  icon: "fa-html5", title: "SUCCESS",
+    description: "How We Help You To Sell Your Product",
+    detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!",
+    iconBlock: [
+      {id: 1, icon: "fa-html5", title: "HTML5 &amp; CSS3", description: "Has ne tritani atomorum conclusionemque, in dolorum volumus cotidieque eum. At vis choroneglegentur iudico"},
+      {id: 2, icon: "fa-bolt", title: "Easy to Use", description: "Cu vero ipsum vim, doctus facilisi sea in. Eam ex falli honestatis repudiandae, sit detracto mediocrem disputationi"},
+      {id: 3, icon: "fa-tablet", title: "Fully Responsive", description: "Cu vero ipsum vim, doctus facilisi sea in. Eam ex falli honestatis repudiandae, sit detracto mediocrem disputationi"},
+      {id: 4, icon: "fa-rocket", title: "Parallax Effect", description: "Cu vero ipsum vim, doctus facilisi sea in. Eam ex falli honestatis repudiandae, sit detracto mediocrem disputationi"},
+    ]
+}
 }
--- Desktop/cyberlitics/cyberlitics/src/app/components/header/header.component.ts
+++ Desktop/cyberlitics/cyberlitics/src/app/components/header/header.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  header = {
+    title: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non beatae voluptatum necessitatibus recusandae nemo, officiis ad dignissimos, laborum exercitationem maiores repellat neque quam dolore, provident adipisci dolorem qui. Harum, possimus? Officiis, sint, iste fuga facilis quasi adipisci architecto at tempora soluta, mollitia cum maiores beatae deleniti error consequatur eum neque ipsa. Distinctio voluptas velit rem tenetur quisquam fugit, nulla tempora. Repellat quaerat illum natus repudiandae sed sit, reiciendis, iusto officia, possimus rem dignissimos mollitia corporis fugit earum? Est autem saepe aliquam iure, debitis possimus similique ipsum praesentium vitae maiores esse!",
+    url: "#",
+    buttonText: "START CREATING TODAY"
+  }
 
 }
